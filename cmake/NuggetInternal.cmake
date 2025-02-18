df40ce4246c83cdd803bc6b612eb626abadf60c7
studyztp <studyztp@gmail.com>
2025-02-17 18:56:33 -0800

    add util in nugget library to read from txt file so and form list. this will help to autometic the process of creating LLC command using the check-cpu-features util and also make creating nuggets easier

--- cmake/NuggetInternal.cmake
+++ cmake/NuggetInternal.cmake
@@ -1,3 +1,19 @@
+function(nugget_read_list OUT_RIDS FILE_PATH)
+    set(out_list "")
+    file(READ ${FILE_PATH} CONTENT)
+    
+    # First replace newlines with semicolons
+    string(REGEX REPLACE "\n" ";" CONTENT "${CONTENT}")
+    # Then split on spaces
+    string(REGEX REPLACE "[ ]+" ";" CONTENT "${CONTENT}")
 
+    foreach(ITEM ${CONTENT})
+        if(NOT "${ITEM}" STREQUAL "")
+            list(APPEND out_list ${ITEM})
+        endif()
+    endforeach()
+
+    set(${OUT_RIDS} ${out_list} PARENT_SCOPE)
+endfunction()
 
 
